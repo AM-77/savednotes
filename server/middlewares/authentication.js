fcdcb381f287c7683e823bbb0be05c59382f069c
AM-77 <amine.griche77@gmail.com>
2019-12-29 21:47:08 +0100

    added check if the token is expiared or not

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -3,8 +3,12 @@ const jwt = require("jsonwebtoken")
 const authentication = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
-        req.verified_token = jwt.verify(token, process.env.JWT_KEY || "15zM4X4S5s0s8E1ApOk4r")
-        next()
+        if (jwt.decode(token).exp > (Date.now() / 1000)) {
+            req.verified_token = jwt.verify(token, process.env.JWT_KEY || "15zM4X4S5s0s8E1ApOk4r")
+            next()
+        } else {
+            res.status(401).json({ message: "Authentication Failed.", error: "Token Expaired." })
+        }
     } catch (error) {
         res.status(401).json({ message: "Authentication Failed." })
     }
