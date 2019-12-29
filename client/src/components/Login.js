c9a632bb88080c8c3d13ce5f6beb8e10222142d9
AM-77 <amine.griche77@gmail.com>
2019-12-29 21:50:34 +0100

    added the login comp

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Axios from 'axios'
+import { login } from './../redux-store/action-creators/authenticationActionCreator'
+import Message from './Message'
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div className="login-container">
+import { BASE_URL } from '../helpers/helper'
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: null,
+            email: null,
+            password: null
+        }
+    }
+
+    onInputChange = e => {
+        let name = e.target.name
+        let value = e.target.value
+        this.setState(({ [name]: value }))
+    }
+
+    onLogin = e => {
+        e.preventDefault()
+
+        // Do some inputs validations
+
+        if (this.state.email === "" || this.state.password === "") {
+            this.show_message({ type: "error", content: "Please Enter Your Email & Password." })
+        } else {
+            Axios.post(BASE_URL + "/login", { email: this.state.email, password: this.state.password })
+                .then(res => {
+                    if (res.data) {
+                        localStorage.setItem("token", res.data.token)
+                        this.props.login(res.data.token, res.data.user)
+                        this.props.history.push("/")
+                    } else {
+                        this.show_message({ type: "error", content: "User Does Not Exist. Please Create An Account, and Retry." })
+                    }
+                })
+                .catch(err => this.show_message({ type: "error", content: err.message }))
+        }
+    }
+
+    show_message = message => this.setState(({ message }), () => { setTimeout(() => { this.setState(({ message: null })) }, 3000) })
+
+
+    render = () => {
+        if (localStorage.getItem("token")) {
+            return <Redirect to="/" />
+        } else {
+            return (<div className="login-container">
                 <div className="login-logo">
                     <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="The saved notes logo" />
                     <h2 className="title">Saved Notes</h2>
                 </div>
                 <div className="login-form">
-                    <form>
+                    <form method="post">
+                        {this.state.message ? <Message {...this.state.message} /> : null}
                         <div className="login-input">
                             <label htmlFor="email">Your email</label>
                             <input id="email" className="login-input" name="email" type="email" onChange={this.onInputChange} />
@@ -23,7 +73,14 @@ export default class Login extends Component {
                         <Link to="/subscribe">Subscribe now.</Link>
                     </form>
                 </div>
-            </div>
-        )
+            </div>)
+        }
     }
 }
+
+const mapStateToProps = (store) => ({ ...store })
+const dispatchStateToProps = (dispatch) => ({
+    login: (token, user) => dispatch(login(token, user))
+})
+
+export default connect(mapStateToProps, dispatchStateToProps)(Login)
