3de1e644878ff2fb93894e99032ba0242dabba60
AM-77 <amine.griche77@gmail.com>
2019-12-29 21:51:22 +0100

    added the reduxStore and the Logged component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import ReduxStore from './redux-store'
 
 import Subscribe from './components/Subscribe'
 import Login from './components/Login'
 import Home from './components/Home'
+import Logged from './components/Logged'
 
 export default class App extends Component {
     render() {
         return (
-            <div className="app-container">
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/subscribe" component={Subscribe} />
-                        <Redirect to="/" />
-                    </Switch>
-                </Router>
-            </div>
+            <ReduxStore>
+                <div className="app-container">
+                    <Router>
+                        <Switch>
+                            <Route path="/login" component={Login} />
+                            <Route path="/subscribe" component={Subscribe} />
+                            <Logged>
+                                <Route exact path="/" component={Home} />
+                                <Redirect to="/" />
+                            </Logged>
+                        </Switch>
+                    </Router>
+                </div>
+            </ReduxStore>
         )
     }
 }
