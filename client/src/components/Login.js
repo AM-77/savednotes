67bf02565cec5484c4fbb7d8f7556905f4dba4a1
AM-77 <amine.griche77@gmail.com>
2020-01-02 22:48:06 +0100

    added verfications, confirmations ...

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,79 +3,97 @@ import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Axios from 'axios'
 import { login } from './../redux-store/action-creators/authenticationActionCreator'
-import Message from './Message'
-
-import { BASE_URL } from '../helpers/helper'
+import main_logo from './../assets/images/main-logo.png'
+import { BASE_URL, validate_email } from '../helpers/helper'
 
 class Login extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            message: null,
-            email: null,
-            password: null,
+            notif: null,
+            email: "",
+            password: "",
             logged: false,
             looking_for_user: false
         }
     }
 
-    onInputChange = e => {
+    on_input_change = e => {
         let name = e.target.name
         let value = e.target.value
         this.setState(({ [name]: value }))
     }
 
-    onLogin = e => {
+    on_login = e => {
         e.preventDefault()
 
-        // Do some inputs validations
-
         if (this.state.email === "" || this.state.password === "") {
-            this.show_message({ type: "error", content: "Please Enter Your Email & Password." })
+            this.setState({ notif: { message: "Please Enter Your Email & Password.", type: "error" }, looking_for_user: false }, () => {
+                setTimeout(() => { this.setState({ notif: null }) }, 1700)
+            })
         } else {
             this.setState({ looking_for_user: true })
-            Axios.post(BASE_URL + "/login", { email: this.state.email, password: this.state.password })
-                .then(res => {
-                    if (res.data) {
-                        this.props.login(res.data.token, res.data.user)
-                        localStorage.setItem("token", res.data.token)
-                        this.setState({ logged: true })
-                    } else {
-                        this.show_message({ type: "error", content: "User Does Not Exist. Please Create An Account, and Retry." })
-                    }
+            if (validate_email(this.state.email)) {
+                Axios.post(BASE_URL + "/login", { email: this.state.email, password: this.state.password })
+                    .then(res => {
+                        if (res.data) {
+                            this.props.login(res.data.token, res.data.user)
+                            localStorage.setItem("token", res.data.token)
+                            this.setState({ logged: true })
+                        } else {
+                            this.setState({ notif: { message: "User Does Not Exist.", type: "error" }, looking_for_user: false }, () => {
+                                setTimeout(() => { this.setState({ notif: null }) }, 1700)
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        this.setState({ notif: { message: err.response.data.message, type: "error" }, looking_for_user: false }, () => {
+                            setTimeout(() => { this.setState({ notif: null }) }, 1700)
+                        })
+                    })
+            } else {
+                this.setState({ notif: { message: "Please Enter A Valid Email.", type: "error" }, looking_for_user: false }, () => {
+                    setTimeout(() => { this.setState({ notif: null }) }, 1700)
                 })
-                .catch(err => this.show_message({ type: "error", content: err.message }))
+            }
         }
     }
 
-    show_message = message => this.setState(({ message }), () => { setTimeout(() => { this.setState(({ message: null })) }, 3000) })
-
-
     render = () => {
-        console.log(this.props)
         if (localStorage.getItem("token") || this.props.auth.logged) {
             return <Redirect to="/" />
         } else {
             return (<div className="login-container">
                 <div className="login-logo">
-                    <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="The saved notes logo" />
+                    <img src={main_logo} alt="The saved notes logo" />
                     <h2 className="title">Saved Notes</h2>
                 </div>
+                {
+                    this.state.notif ?
+                        <div className={`notify-user ${this.state.notif.type}`}>
+                            <p>{this.state.notif.message}</p>
+                        </div>
+                        :
+                        null
+                }
                 <div className="login-form">
                     <form method="post">
-                        {this.state.message ? <Message {...this.state.message} /> : null}
                         <div className="login-input">
-                            <label htmlFor="email">Your email</label>
-                            <input id="email" className="login-input" name="email" type="email" onChange={this.onInputChange} />
+                            <label htmlFor="email">Your email:</label>
+                            <input id="email" className="login-input" name="email" type="email" onChange={this.on_input_change} />
                         </div>
                         <div className="login-input">
-                            <label htmlFor="password">Your password</label>
-                            <input id="password" className="login-input" name="password" type="password" onChange={this.onInputChange} />
-                            {this.state.looking_for_user ? <div>loading ...</div> : null}
+                            <label htmlFor="password">Your password:</label>
+                            <input id="password" className="login-input" name="password" type="password" onChange={this.on_input_change} />
                         </div>
-                        <button type="submit" onClick={this.onLogin} className="login-button">Sign In</button>
-                        <Link to="/subscribe">Subscribe now.</Link>
+                        <div className="loading-user">
+                            {this.state.looking_for_user ? <div className="loading"><span>.</span><span>.</span><span>.</span></div> : null}
+                        </div>
+                        <div className="login-foot">
+                            <Link to="/subscribe">Subscribe now.</Link>
+                            <button type="submit" onClick={this.on_login} className="login-button">Sign In</button>
+                        </div>
                     </form>
                 </div>
             </div>)
@@ -83,9 +101,9 @@ class Login extends Component {
     }
 }
 
-const mapStateToProps = (store) => ({ ...store })
-const dispatchStateToProps = (dispatch) => ({
+const map_state_to_props = (store) => ({ ...store })
+const dispatch_state_to_props = (dispatch) => ({
     login: (token, user) => dispatch(login(token, user))
 })
 
-export default connect(mapStateToProps, dispatchStateToProps)(Login)
+export default connect(map_state_to_props, dispatch_state_to_props)(Login)
