0d358eb10029ed0807676cd4d292653e2d046ad6
AM-77 <amine.griche77@protonmail.ch>
2020-10-02 19:55:28 +0100

    remove useless files

--- server/helpers.js
+++
@@ -1,3 +0,0 @@
-const get_date = () => `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
-
-module.exports = get_date
