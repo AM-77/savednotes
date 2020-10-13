d735b07a13cf9c17f58997b7163226944d35ebff
AM-77 <amine.griche77@protonmail.ch>
2020-10-13 23:16:04 +0100

    update the server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3300;
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log('[+] The savednotes-server is running on port ' + PORT));
+server.listen(PORT, () =>
+  console.log(`[+] The savednotes server is running on port ${PORT}`),
+);
+
+module.exports = server
