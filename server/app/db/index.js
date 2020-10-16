23e4097baa5000fbf19445bc695c12a4a902c1d2
AM-77 <amine.griche77@protonmail.ch>
2020-10-16 07:29:56 +0100

    change db connection

--- server/app/db/index.js
+++ server/app/db/index.js
@@ -1,59 +1,18 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
+require("dotenv").config();
 
-// Create a pool
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  queueLimit: 100,
+const conn = mysql.createConnection({
   host: process.env.DB_HOST || "127.0.0.1",
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "savednotes",
-  connectTimeout: 10000,
-  waitForConnections: true,
-  acquireTimeout: 10000,
-  debug: false,
 });
 
-// Select the database
-pool.getConnection((err, conn) => {
-  if (err) throw err;
-  conn.query(`USE ${process.env.DB_NAME}`, (error) => {
-    if (error) throw error;
-    conn.release();
-  });
-});
+conn.connect();
 
-// Returns a connection to the database
-const getConnection = (callback) => {
-  pool.getConnection((err, conn) => {
-    if (err) throw err;
-    callback(err, conn);
-  });
-};
+const query = (sql, callback) => {
+  conn.query(sql, (error, results) => callback(error, results));
+}
 
-// Helper function for querying the database; releases the database connection
-const query = (queryString, params, callback) => {
-  getConnection((err, conn) => {
-    if (err) return callback(err);
-    return conn.query(queryString, params, (error, rows) => {
-      conn.release();
-      if (error) throw error;
-      return callback(error, rows);
-    });
-  });
-};
-
-// Heartbeat function to keep the connection to the database up
-const keepAlive = () => {
-  getConnection((err, conn) => {
-    if (err) throw err;
-    conn.ping();
-    conn.release();
-  });
-};
-
-// Set up a keepalive heartbeat
-setInterval(keepAlive, 30000);
-
-exports.query = query;
+module.exports = { query };
